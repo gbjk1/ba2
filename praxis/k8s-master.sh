32906cac5bff5390532cd3a44acc24c5ac6107ab
gbjk1 <klausguntram@icloud.com>
2024-05-22 00:42:05 +0200

    state

--- praxis/k8s-master.sh
+++ praxis/k8s-master.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
 kubeadm init --pod-network-cidr=10.0.0.0/24 --apiserver-advertise-address 192.168.20.3
--- praxis/k8s-worker.sh
+++ praxis/k8s-worker.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-kubeadm join 192.168.20.3:6443 --token [token] \
-	--discovery-token-ca-cert-hash [token]
+kubeadm join 192.168.20.3:6443 --token [token from kubeadm init] --discovery-token-ca-cert-hash [token from kubeadm init]
