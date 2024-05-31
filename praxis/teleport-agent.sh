c4e64efb830e74f2c276fe6ae3cecdd4a41067ce
gbjk1 <klausguntram@icloud.com>
2024-06-01 01:43:36 +0200

    format

--- praxis/teleport-agent.sh
+++ praxis/teleport-agent.sh
@@ -4,4 +4,5 @@ kubectl create namespace teleport-agent
 
 helm repo add teleport https://charts.releases.teleport.dev 
 helm repo update
-helm helm install teleport-agent teleport/teleport-kube-agent -f nebhis-cluster-values.yaml --version 14.3.3 --create-namespace --namespace teleport-agent
+helm helm install teleport-agent teleport/teleport-kube-agent -f nebhis-cluster-values.yaml \
+  --version 14.3.3 --create-namespace --namespace teleport-agent
