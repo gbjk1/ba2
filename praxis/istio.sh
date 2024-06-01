aa314b9a551daba710f28b8d0de65844974eae9c
gbjk1 <klausguntram@icloud.com>
2024-06-01 02:19:51 +0200

    istio

--- praxis/istio.sh
+++ praxis/istio.sh
@@ -5,6 +5,7 @@ kubectl create namespace istio-system
 helm repo add istio https://istio-release.storage.googleapis.com/charts
 helm repo update
 helm install istio-base istio/base -n istio-system
+helm install istiod istio/istiod -n istio-system --wait
 
 kubectl create namespace istio-ingress
 helm install istio-ingress istio/gateway -n istio-ingress --wait
